c4aaf451541ddbd1f3a5daff3bb4c9ce0678a137
Jhonfred Galarraga <galarragajhonfred@gmail.com>
2021-04-20 16:22:33 -0400

    Game function

--- main.py
+++ main.py
@@ -16,24 +16,36 @@ def normalize(s):
 
 
 # Generate the words
-def word():
+def words():
     number_word = random.randint(0, 170) # Generate a random number between 0 and 170
     with open('./archivos/data.txt', 'r', encoding='utf-8') as data:
         # The method str.rstrip() removes the line breaks (\n)
         words = {str.rstrip(word) for word in data} # Generate a dictionary with the words in data 
         words = dict(enumerate(words)) # Add a key to the dictionary
-    word = [lyrics for lyrics in normalize(words.get(number_word))]
+    word = [letter for letter in normalize(words.get(number_word))] # Select a word at random and separate its letters into a list.
     return word
-    
- 
 
-def game():
+
+def interface(word, clue, oportunities):
     pass
 
 
+def game():
+    word = words()
+    oportunities = 10
+    clue = ['_' for i in word]
+    print("".join(word))
+    while clue != word:
+        print("".join(clue))
+        letter = input('Enter a letter: ')
+        for i, l in enumerate(word):
+            if l.lower() == letter.lower():
+                clue[i] = l
+
+
 def main():
-    print(word())
+    game()
 
 
 if __name__ == '__main__':
-    main()
+    game()
